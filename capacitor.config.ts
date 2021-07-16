b4511031e8481482cf8e99baf17102f438773deb
nivaldo <nivaldofe.jr@gmail.com>
2021-07-16 14:46:31 -0400

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-pokedex',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { PokemonService } from './../services/pokemon.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  offset = 0;
+  pokemon = [];
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
 
-  constructor() {}
+  constructor(private pokemonService: PokemonService) {}
 
+  ngOnInit(){
+    this.loadPokemon();
+  }
+
+  loadPokemon(loadMore = false, event?){
+    if(loadMore){
+      this.offset += 25;
+    }
+    this.pokemonService.getPokemon(this.offset).subscribe(res =>{
+      console.log('resultado: ', res);
+        this.pokemon = [...this.pokemon, ...res];
+    
+        if (event){
+          event.target.complete();
+        }
+
+        if(this.offset == 865){
+          this.infinite.disabled = true;
+        }
+    })
+  }
+
+  onSearchChange(e){
+    let value = e.detail.value;
+    
+    if (value == ''){
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+
+    this.pokemonService.findPokemon(value).subscribe(res =>{
+      this.pokemon = [res]
+    }, err =>{
+      this.pokemon = []
+    });
+  }
 }
